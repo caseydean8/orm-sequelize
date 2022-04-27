ad206491fe0af642cd6cabd9e9dc82c5d395e803
Casey Carroll <caseydean8@gmail.com>
2022-04-27 19:24:23 -0400

    Added express post route.

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const { sequelize } = require('./models')
+const express = require("express");
 
-async function main(){
-  await sequelize.sync()
-}
+const { sequelize, User } = require("./models");
 
-main()
+const app = express();
+app.use(express.json());
+
+app.post("/users", async (req, res) => {
+  const { name, email, role } = req.body;
+
+  try {
+    const user = await User.create({ name, email, role });
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+app.listen({ port: 5000 }, async () => {
+  console.log("Server up on http://localhost:5000");
+  await sequelize.sync({ force: true });
+  console.log("Database synced!");
+});
--- models/user.js
+++ models/user.js
@@ -1,10 +1,23 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    role: DataTypes.STRING
-  }, {});
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  }, {
+    sequelize,
+    tableName: 'users',
+    modelName: 'User'
+  });
   User.associate = function(models) {
     // associations can be defined here
   };
